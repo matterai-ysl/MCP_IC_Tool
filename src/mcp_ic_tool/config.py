e2301d15dd102d07a26cd9218bb2831e2852b669
sicas-songlin <yusonglin22@mails.ucas.ac.cn>
2025-09-19 11:36:02 +0800

    fix a bug

--- src/mcp_ic_tool/config.py
+++ src/mcp_ic_tool/config.py
@@ -3,17 +3,17 @@ from typing import Optional
 BASE_URL = "http://localhost:8130"
 DOWNLOAD_URL = "/data/home/ysl9527/vasp_calculations"
 MCP_PORT = 8130
-# VASP_SERVER_BASE_URL = "http://localhost:8135"
-VASP_SERVER_BASE_URL = "http://47.99.180.80/vasp_server"
+VASP_SERVER_BASE_URL = "http://localhost:8135"
+VASP_SERVER_BASE_File_URL = "https://47.99.180.80/vasp_server"
 VASP_SERVER_PORT = 8135
 from pathlib import Path
 
 
 def get_download_url(path:str):
-    return f"{VASP_SERVER_BASE_URL}/vasp/download/{Path(path).relative_to(DOWNLOAD_URL).as_posix()}"
+    return f"{VASP_SERVER_BASE_File_URL}/vasp/download/{Path(path).relative_to(DOWNLOAD_URL).as_posix()}"
 
 def get_static_url(path:str):
-    return f"{VASP_SERVER_BASE_URL}/vasp/static/{Path(path).relative_to(DOWNLOAD_URL).as_posix()}"
+    return f"{VASP_SERVER_BASE_File_URL}/vasp/static/{Path(path).relative_to(DOWNLOAD_URL).as_posix()}"
 
 def get_env(name: str, default: Optional[str] = None) -> Optional[str]:
     return os.environ.get(name, default)
